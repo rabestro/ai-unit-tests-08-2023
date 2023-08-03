2a54296ccb191ee788ec171419e36d3fdfca2b25
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-03 18:22:39 +0300

    add RomanToArabicConverterDiffblueTest

---
+++ src/test/java/org/epam/roman/RomanToArabicConverterDiffblueTest.java
@@ -0,0 +1,23 @@
+package org.epam.roman;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class RomanToArabicConverterDiffblueTest {
+    /**
+     * Method under test: {@link RomanToArabicConverter#applyAsInt(String)}
+     */
+    @Test
+    void testApplyAsInt() {
+        // Arrange
+        RomanToArabicConverter romanToArabicConverter = new RomanToArabicConverter();
+        String value = "42";
+
+        // Act
+        int actualApplyAsIntResult = romanToArabicConverter.applyAsInt(value);
+
+        // Assert
+        assertEquals(0, actualApplyAsIntResult);
+    }
+}
