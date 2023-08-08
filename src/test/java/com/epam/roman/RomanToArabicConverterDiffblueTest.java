2ae8ed2a14f035b4922ea9f654f88b0f656e26d7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-08 21:03:01 +0300

    Reorder import statements in RomanToArabicConverterDiffblueTest
    Updated the order of the import statements for consistency in the RomanToArabicConverterDiffblueTest class. No functional changes are included in this commit. Also, added unit tests for the FlipFlopPredicate class in a new separate file, to provide better test coverage and ensure the code behaves as expected in different scenarios.

--- src/test/java/com/epam/roman/RomanToArabicConverterDiffblueTest.java
+++ src/test/java/com/epam/roman/RomanToArabicConverterDiffblueTest.java
@@ -1,8 +1,8 @@
 package com.epam.roman;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class RomanToArabicConverterDiffblueTest {
     /**
