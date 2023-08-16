8ebaed32d96b3757c62584ec9864594c0122d66e
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-16 20:41:42 +0300

    Remove deprecated test guidelines, add new tests for 'Swapping', 'Overing', 'Subtraction', 'Multiplication' and 'Division' methods
    Removed the previously included 'junit5-white-box.md' which contained older testing instructions. New white box tests have been added for the ForthWord module's 'Swapping', 'Overing', 'Subtraction', 'Multiplication' methods along with "DiffBlue" and "Ai" tests for 'Division' and 'Addition' methods. These changes were made to update our test suite with latest practices and increase the coverage of our units in the ForthWord module.

---
+++ src/test/java/com/epam/forth/word/MultiplicationWhiteBoxAiTest.java
@@ -0,0 +1,66 @@
+package com.epam.forth.word;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MultiplicationWhiteBoxAiTest {
+
+    @Test
+    @DisplayName("When the stack contains two values, it should multiply them")
+    void multiplyingTwoValues() {
+        var multiplication = new Multiplication();
+        var stack = new ArrayDeque<Integer>();
+        stack.push(10);
+        stack.push(2);
+
+        multiplication.accept(stack);
+        assertThat(stack)
+                .as("After multiplication, the stack should contain the product of the original values")
+                .containsExactly(20);
+    }
+
+    @Test
+    @DisplayName("When the stack contains multiple values, it should multiply the top two and leave the rest")
+    void multiplyingTopTwoValuesFromMultipleValues() {
+        var multiplication = new Multiplication();
+        var stack = new ArrayDeque<Integer>();
+        stack.push(30);
+        stack.push(20);
+        stack.push(10);
+
+        multiplication.accept(stack);
+        assertThat(stack)
+                .as("After multiplication, the stack should contain the remaining value and the product of the top two original values")
+                .containsExactly(200, 30);
+    }
+
+    @Test
+    @DisplayName("An exception should be thrown when trying to multiply values from a stack with less than two values")
+    void throwingExceptionWhenMultiplyingFromLessThanTwoValues() {
+        var multiplication = new Multiplication();
+        var stack = new ArrayDeque<Integer>();
+        stack.push(10);
+        
+        assertThatThrownBy(() -> multiplication.accept(stack))
+                .as("An IllegalStateException was expected when multiplying less than two values")
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Multiplication requires that the stack contain at least 2 values");
+    }
+
+    @Test
+    @DisplayName("An exception should be thrown when trying to multiply values from an empty stack")
+    void throwingExceptionWhenMultiplyingFromEmpty() {
+        var multiplication = new Multiplication();
+        var stack = new ArrayDeque<Integer>();
+
+        assertThatThrownBy(() -> multiplication.accept(stack))
+                .as("An IllegalStateException was expected when multiplying on an empty stack")
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Multiplication requires that the stack contain at least 2 values");
+    }
+}
