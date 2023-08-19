8efda2edec8531a28d335b44a3ac288f477965d7
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-08-19 11:03:08 +0300

    Update TrihedralDice tests and create new test class
    Enhanced existing tests in TrihedralDiceWhiteBoxAiTest and created a new test class, TrihedralDiceWhiteBoxA1Test.
    
    In TrihedralDiceWhiteBoxAiTest, a test was added to check whether the object created from the TrihedralDice class is not null. Furthermore, the test that verifies the result of dice roll was modified to be performed 100 times using IntStream, ensuring a more comprehensive check.
    
    TrihedralDiceWhiteBoxA1Test, a new class, was added to handle tests previously performed in TrihedralDiceWhiteBoxAiTest. A single test currently validates that a roll result falls within the correct range (1-3).
    
    The changes aim to improve the organization and thoroughness of the tests, ensuring that the TrihedralDice class functions as expected in all scenarios.

--- src/test/java/com/epam/rockpaperscissors/dice/TrihedralDiceWhiteBoxAiTest.java
+++ src/test/java/com/epam/rockpaperscissors/dice/TrihedralDiceWhiteBoxAiTest.java
@@ -3,18 +3,31 @@ package com.epam.rockpaperscissors.dice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("TrihedralDice White Box AI Test")
 class TrihedralDiceWhiteBoxAiTest {
 
-    @DisplayName("Rolls the default Trihedral dice and verifies the results")
+    @DisplayName("Creating TrihedralDice, expecting non-null:")
     @Test
-    void testRoll() {
+    void creatingTrihedralDiceExpectingNonNull() {
         var dice = new TrihedralDice();
-        var rollResult = dice.roll();
+        assertThat(dice)
+            .as("TrihedralDice object must not be null when created")
+            .isNotNull();
+    }
 
-        assertThat(rollResult)
-                .as("The result '%d' should be in the inclusive range [1, 3]", rollResult)
+    @DisplayName("Rolling the TrihedralDice 100 times, expecting a number in the range 1-3 every time:")
+    @Test
+    void rollingTheTrihedralDiceExpectingNumberInCorrectRange() {
+        var dice = new TrihedralDice();
+        IntStream.range(0, 100).forEach(x -> {
+            var rollValue = dice.roll();
+            assertThat(rollValue)
+                .as("Roll value must be between 1 and 3 (inclusive) for a TrihedralDice")
                 .isBetween(1, 3);
+        });
     }
 }
