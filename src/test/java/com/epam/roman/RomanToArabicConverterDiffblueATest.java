6c1a2254d13beca6659205e73483b1731b84b5d0
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-09-04 20:50:38 +0300

    Add unit tests for RomanToArabicConverter class
    A new unit test case for the RomanToArabicConverter class has been added. The test is specifically for `applyAsInt` method and checks its ability to correctly convert Roman numbers to Arabic. The test uses "42" as an input and expects zero as a result.

---
+++ src/test/java/com/epam/roman/RomanToArabicConverterDiffblueATest.java
@@ -0,0 +1,23 @@
+package com.epam.roman;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RomanToArabicConverterDiffblueATest {
+    /**
+     * Method under test: {@link RomanToArabicConverter#applyAsInt(String)}
+     */
+    @Test
+    void testApplyAsInt() {
+        // Arrange
+        RomanToArabicConverter romanToArabicConverter = new RomanToArabicConverter();
+        String value = "42";
+
+        // Act
+        int actualApplyAsIntResult = romanToArabicConverter.applyAsInt(value);
+
+        // Assert
+        assertEquals(0, actualApplyAsIntResult);
+    }
+}
