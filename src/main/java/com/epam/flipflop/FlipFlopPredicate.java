73dcca2f11d0e1493125b07dcddb7f8d49116e6d
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-09-12 20:51:46 +0300

    The provided commit message does not match the diff provided. Here's an appropriate message:
    "Refactor variable names and add method comments in FlipFlopPredicate
    
    Renamed variables 'lhs' and 'rhs' to 'startPredicate' and 'endPredicate' for clarity. Added comments to describe the logic of the FlipFlopPredicate class."

--- src/main/java/com/epam/flipflop/FlipFlopPredicate.java
+++ src/main/java/com/epam/flipflop/FlipFlopPredicate.java
@@ -4,21 +4,23 @@ import java.util.function.Predicate;
 
 import static java.util.Objects.requireNonNull;
 
-
+/**
+ * Emulates flip-flop logic similar to two-dots flip flop in Perl or Ruby.
+ */
 public final class FlipFlopPredicate<T> implements Predicate<T> {
-    private final Predicate<? super T> lhs;
-    private final Predicate<? super T> rhs;
+    private final Predicate<? super T> startPredicate;
+    private final Predicate<? super T> endPredicate;
     private boolean state;
 
     public FlipFlopPredicate(Predicate<? super T> lhs, Predicate<? super T> rhs) {
-        this.lhs = requireNonNull(lhs);
-        this.rhs = requireNonNull(rhs);
+        this.startPredicate = requireNonNull(lhs);
+        this.endPredicate = requireNonNull(rhs);
     }
 
     @Override
     public boolean test(final T value) {
-        var result = state || lhs.test(value);
-        state = result && !rhs.test(value);
+        var result = state || startPredicate.test(value);
+        state = result && !endPredicate.test(value);
         return result;
     }
 }
