31ce2bf5b5f6e26cb8abac74e86fe7f55635532f
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-09-17 21:44:39 +0300

    Add 'hammingWeight' solution and test cases
    Added the `hammingWeight` method in Solution.java file under leetcode.p191 package, which returns the count of '1's in the binary representation of a number. Used `Integer.bitCount(n)` function for this purpose.
    
    Also added a test class, `SolutionBlackBoxC4Test` that checks the output for both positive and negative numbers, and edge cases like zero, -1, and numbers where only the MSB is set.
    
    Providing a test suite for `hammingWeight` function helps us to ensure that the function is working properly across various edge cases and provides a safety net for future changes.

---
+++ src/main/java/leetcode/p191/Solution.java
@@ -0,0 +1,8 @@
+package leetcode.p191;
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}
